ff379db7da5e04a7f47ae25e751b9ae5ee42a19d
Tom Clark <thrclark@iu.edu>
2017-01-31 17:10:34 -0500

    More framework

---
+++ php/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+<link href="../less/styles.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<div class="container" role="main">
+    <div class="row">
+        <div class="col-md-6">sdfg </div>
+        <div class="col-md-6"> sdfg</div>
+    </div>
+</div>
+<script type="text/javascript" src="../js/jquery.js"></script> 
+<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+</body>
+</html>
